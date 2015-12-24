61deca9532c399e402ba48d701709ed51cc4da3c
Stanley Zheng <dev.stanzheng@gmail.com>
2015-12-23 21:48:25 -0500

    adding part two ...

--- day1/golang/src/day1.go
+++ day1/golang/src/day1.go
@@ -17,17 +17,36 @@ var downlevel = []byte(")")
 // Count the number of floors using string replacement
 // Count the difference
 func main() {
-	part1()
-}
-
-func part1() {
 	file, err := ioutil.ReadFile("../input.txt")
 	if err != nil {
 		fmt.Println("file can't be read")
 	}
 
+	part1(file)
+	part2(file)
+}
+
+func part1(file []byte) {
 	up := bytes.Replace(file, downlevel, blank, -1)
 	down := bytes.Replace(file, upLevel, blank, -1)
-	fmt.Printf("Santa went up %d floors and down %d floors and ended up on the %d floor", len(up), len(down), len(up)-len(down))
+	fmt.Printf("Santa went up %d floors and down %d floors and ended up on the %d floor\n", len(up), len(down), len(up)-len(down))
 
+}
+
+func part2(file []byte) {
+	floorLevel := 0
+	firstTime := true
+	for i := 0; i < len(file); i++ {
+
+		if string(file[i]) == "(" {
+			floorLevel = floorLevel + 1
+		} else {
+			floorLevel = floorLevel - 1
+		}
+		if !firstTime && floorLevel < 0 {
+			fmt.Printf("Santa enters the basement at char %d \n", i+1)
+			break
+		}
+		firstTime = false
+	}
 }
