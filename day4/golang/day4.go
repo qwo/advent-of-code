d9f2bc44d6c1b43ddf9edb0eccb2e1463cc33288
Stanley Zheng <szheng@thisisgrow.com>
2016-01-03 22:36:52 -0500

    cleaning up the code

--- day4/golang/day4.go
+++ day4/golang/day4.go
@@ -13,9 +13,10 @@ func main() {
 	found := false
 	i := 0
 	for !found {
-		hash := md5.Sum([]byte(secretKey + strconv.Itoa(i)))
-
-		check := (fmt.Sprintf("%x", hash[0:3]))[0:5]
+		val := secretKey + strconv.Itoa(i)
+		hash := md5.Sum([]byte(val))
+		check := fmt.Sprintf("%x", hash[0:3])
+		check = check[0:5]
 		if check == marker {
 			fmt.Printf("Found at %d", i)
 			found = true
