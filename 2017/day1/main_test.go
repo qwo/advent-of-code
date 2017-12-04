26c925f6e71e2752d82e2640b9fb3093d9d9cb22
Stanley Zheng <dev.stanzheng@gmail.com>
2017-12-04 02:24:13 -0500

    updating day1

--- 2017/day1/main_test.go
+++ 2017/day1/main_test.go
@@ -4,8 +4,30 @@ import "testing"
 
 
 func TestTimeConsuming(t *testing.T) {
-	m:=make(map[string]int){{"1122": 3 }}
-    if testing.Short() {
-        t.Skip("skipping test in short mode.")
+    m := map[string]int{
+        "1122": 3,
+        "1111": 4,
+        "1234": 0,
+        "91212129": 9,
+    }
+
+    m2 := map[string]int {
+        "1212": 6,
+        "1221": 0,
+        "123425": 4,
+        "123123": 12,
+        "12131415": 4,
+
+    }
+
+    for in, expect:=range(m) {
+        if findSum(in) != expect {
+            panic("Error!")
+        }
+    }
+    for in, expect:=range(m2) {
+        if findHalfwaySum(in) != expect {
+            panic("Error!")
+        }
     }
 }
