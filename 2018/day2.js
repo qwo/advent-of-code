c44ba3b4aa41b2eb7907186cf035a08802efa582
Stanley Zheng <dev.stanzheng@gmail.com>
2018-12-02 19:50:03 -0500

    ugly node solution

---
+++ 2018/day2.js
@@ -0,0 +1,332 @@
+const assert = require('assert')
+input = `ybruvapdgixszyckwtfqjonsie
+mbruvapxghslyyckwtfqjonsie
+mbruvapdghslzyckwtkujonsie
+rwruvapdghxlzyckwtfqjcnsie
+obruvapdgtxlzyckwtfqionsie
+lbruvapdghxqzyckwtfqjfnsie
+mbrunapdghxlzyccatfqjonsie
+mbruvapdghxlzyokltfqjdnsie
+ybruvapdghxlzmckwtfqjmnsie
+mbruwaadghxdzyckwtfqjonsie
+muruvapdghxlzyckvtfqjonsim
+mbruvapdghxlkyckwtxqjonjie
+mbruvaqdghxlzyckwtfqjrnnie
+mwruvapdghdlzyckttfqjonsie
+mbruvapdgtelzyckwxfqjonsie
+mbruvapdohxlzvckwtfqjonhie
+mbrugapdgbxlzyckwtfqjynsie
+mbruvapdghxlzyckwtlqjonjiu
+mbruvapwghxlzyckwafqjonbie
+wbruvapdghxlhyckwtfqjonsii
+mbruvapdghxlzyckwtcqnonsiq
+mbyuvapighxlzybkwtfqjonsie
+mbrrvapdghxvzyckwtfqjonsio
+mhruvapdghrlzyckwtfzjonsie
+mtruvapvghxlzyckwtfnjonsie
+mmrlhapdghxlzyckwtfqjonsie
+mbruvapdgpxlzyjkwtfqjovsie
+mbrucapdghxlzymkwtzqjonsie
+mbeuvafdghxlzyckwtfqjonwie
+mbruvapcghxlayckwtfqjonsii
+mbruvabdghxlzyckwtfqyansie
+mbruvjpdghxlzyckwtfqgfnsie
+lbruvapdghxlzyckwtfqjonriv
+mbrupapdghxlzycjwtfqronsie
+mbpuvapdthxlzymkwtfqjonsie
+mbiuvapdgixlzyckwxfqjonsie
+mbruvapdghxyzyckwtfcjonsbe
+mbrurapkghxlzyckwtfqjonzie
+mbrufapdrhxlzyciwtfqjonsie
+mbruvapdghxlzbckwtfqjoisae
+ubruhapdghxlzuckwtfqjonsie
+mbruvapdjhulzyckwtfqjonshe
+mbruwapdgyxlzyckntfqjonsie
+mwruvapdghplzyckwtfqjonsme
+mbruvapjghtlzyckwtfqgonsie
+pbruvapdghhlzyckwtfrjonsie
+mbruvgpdihxqzyckwtfqjonsie
+mbruvahdohxlzyckwtfijonsie
+ibuuvapdghxlzyckwtfqjofsie
+mbruvandghxlzyckwtfqjrnxie
+mbrjvlpdghxlzyckwgfqjonsie
+mbruvapogfxlzyckotfqjonsie
+mbruvrpdghxlzyckutfejonsie
+mbruvbpdghxlzyhkwtfqjonsip
+mbruvapdghxlzyckmnfqjensie
+mbruvapdghvlzyckwtfqjowsix
+mbruvakdgholzwckwtfqjonsie
+mbruvapdghxlzackwtfqconsae
+mbruvapdghxlzyqvwtfqjlnsie
+mprrvapdgfxlzyckwtfqjonsie
+mbrunacdghxlhyckwtfqjonsie
+obruvapdgsxlzyckwtfqjonvie
+murcvapdghslzyckwtfqjonsie
+mbruvapdghxlzyzkwmftjonsie
+mbrwvapdgtvlzyckwtfqjonsie
+mbxuvapdghxlzqcnwtfqjonsie
+mbruvaddghxboyckwtfqjonsie
+mhruvwndghxlzyckwtfqjonsie
+mbrdvapdghxlzyckwmpqjonsie
+mbruvapdgyxlzyckizfqjonsie
+mbruvapdghxlzlckwtfqeowsie
+mbruvbpdgrxlzyckwtfqjonsxe
+mbruqapoghxlzyckwtvqjonsie
+mbouhapdghmlzyckwtfqjonsie
+mbruvapjghxidyckwtfqjonsie
+mbsuvapkghxlkyckwtfqjonsie
+mbruvlpdghxlzycrwtfqjonsis
+mcrueapdghxlzyckwtfqjynsie
+muruvapngbxlzyckwtfqjonsie
+mbruvapdghxlzycawtfyjojsie
+mbruvbpdghxczyjkwtfqjonsie
+ybduvapdghxnzyckwtfqjonsie
+mbruvbpdghxlzyckwtfbjousie
+mbouvapdghxlzycbwtfqponsie
+mbruvaedghplzycgwtfqjonsie
+mbrhvapdghxlzyckytfqjgnsie
+mbruvapdqbxleyckwtfqjonsie
+mbruvapddhhldyckwtfqjonsie
+mbruvapdghxlwrckwtfqjondie
+mbruvapdmhxlzyckwtfqkonsve
+xbbuvapdghxlzyckwtfkjonsie
+mbruvapdghxlzyckwcfqjunkie
+mbruvapdghxlzyckwtfqxonfib
+mbrtvapkghxlzyckwtfqeonsie
+mbruvazdghxldymkwtfqjonsie
+kbruvapddhxlzfckwtfqjonsie
+mbouvapdghxlpyckwtfqjoosie
+mbauvapdghxlzyckwtfqjszsie
+mbruvapdghtlzyckntfqtonsie
+mbruvipdggxlzbckwtfqjonsie
+mbruqapdghrlzyckwtfqjznsie
+myruvacdghxlzyckwifqjonsie
+mbruvapdghxlzuckwtfkjocsie
+mwjuvapdghxlzyckwtfqjonsxe
+mbruvapxghxlzickwtfqjobsie
+mbrupapdghxtlyckwtfqjonsie
+meruvapdjjxlzyckwtfqjonsie
+mbruvkodghxlzyckwofqjonsie
+mbruvapdgexlzyckwtgkjonsie
+mbruvapwghxlzyckwtcqjonsiw
+mbruvapdghxlzykkwtfqtoxsie
+mbruvapdahxlzycgwtfwjonsie
+mbruvapdgwxlhyckhtfqjonsie
+mbruvapbghxlzycbhmfqjonsie
+mbruvapdghxvzyzkwtfqjonsin
+mbrcvapdqhxlzyckwyfqjonsie
+zbruvaxdghxlzyckwgfqjonsie
+mtruvapdghxlilckwtfqjonsie
+bbruvapdghxlzyckwtfmjonsxe
+mbruvajdghxlzyckwtfqfwnsie
+mbruvapdgkxlzyckwtfqionpie
+rbruvapdghxlryckwdfqjonsie
+mbruvandghxlzyckwmfvjonsie
+mbruvahdghxlzeckwtfqjonsme
+mbruvnpcghxlzyckwtfqjobsie
+mbruvapdghqlzyckwtfbjonsiy
+mbruvavdghxlzyckwufqjodsie
+mbruvapdghxlzyckwtfzmovsie
+mbruvlpdghxuzyckwtfqjoesie
+mbruvopdghxlzycwwtfqjansie
+obruvapdghglzybkwtfqjonsie
+mbpuvlpdghxlcyckwtfqjonsie
+mbruvaidghxlzyckwtfmjonoie
+mbruvapdihxzzyckwtfqjonsiy
+mbquvapdghxlzyckwtfqconsme
+mbruvapdghslzyckqtfqjojsie
+mbrzdapdghxmzyckwtfqjonsie
+mwruvapdghxozyckwtfqjonsxe
+muruvapdgfxlzyckwtfqjojsie
+wtruvapdghxlzyckvtfqjonsie
+mbruvapdghxlzyckysfqjxnsie
+mbruvrpdghxczyckktfqjonsie
+mbquvapdghxlryckwtfqjonsne
+mbruvapdghflzycvwtfqjpnsie
+mbruvapughclzyckwtfqjonsin
+mbrhvapdghxlpyckwtfqjonsre
+mbruvapdgtxlzyckwtfqjoosit
+mbrupapnghxhzyckwtfqjonsie
+mmvuvapdvhxlzyckwtfqjonsie
+mbruvaptghxlzyckwtfqjotsse
+mgruvapvghxlzyckwtfqjonsix
+mbrupapdghxszyckwtfqjunsie
+mbruvkpdghelzyckwtfqjpnsie
+mbruvrrdghjlzyckwtfqjonsie
+mbruvapdghnlzyckwtfkjonsze
+mbruvwpdghxlzyckwtfqhoysie
+mbrsvapdfhxlzyckwtfqjobsie
+mbruvapdgexezymkwtfqjonsie
+ybruvapdghxlzyckwtfqxonsiw
+mrruvapdghxdzyckwtfqjossie
+mbruvapdghtlzyckwtfqconsiu
+mbrpvapdghxlzlckwpfqjonsie
+mbruvjpdghslzyckwtfqjjnsie
+mhruvapoghxlzyckwtfvjonsie
+mbrubqpdghvlzyckwtfqjonsie
+mbruvapdghxlzackwtfqconsiw
+mbruvapdgnxlzkckwtfqjdnsie
+mbrudapgghelzyckwtfqjonsie
+mbruvapdghxlzlakwbfqjonsie
+mbpuvapdghxlzyckwtuqjonjie
+abruvapdghxlzykkwtfqjonzie
+mbrupupdghxlsyckwtfqjonsie
+mbrsvupdghxlzyckwtfqjonkie
+mxruvgpdghxllyckwtfqjonsie
+mbrnvapdghxlzycbwtfqfonsie
+mbrbxapdghxlzyckttfqjonsie
+mbnuvapdghxlzyxkwtmqjonsie
+mbrfvapdghjlzickwtfqjonsie
+mbhuvupdghxlzyxkwtfqjonsie
+mbrcvapdghxluyckwtfqjznsie
+mbruvapdghxlzyckwofqjoxsiz
+mbrevapdghxloyckwtfqjonnie
+mbruvipdghnlzyckwtfqjopsie
+mbxxvaptghxlzyckwtfqjonsie
+mbruvcpdghxlztckwtjqjonsie
+mqruvlpdghxlzyckotfqjonsie
+mbruvapdgqxlzyckwtfqjpvsie
+mbruvapdgvxlzyjkwtfqjbnsie
+mbruvapdghxlgyckwtfqcocsie
+mbruvapdghxkwyckwtfqjoqsie
+mbrgvavdghxlzyckwxfqjonsie
+qbruqapdgvxlzyckwtfqjonsie
+mbauvapdghxlzgckwtfqjunsie
+mbruvapdgdxluyckwtfqjoosie
+mbruvapdghxlzykkwtfqwobsie
+mbruvapdghxlzhcnwtfqjonqie
+mbruvapdghxlzycbhmfqjonsie
+mbruvapdghxluyczwtfqjontie
+mbruvapnghxlzyckwnfqjonbie
+moruvapdghxlzcckwtfqponsie
+mbruvapfgxxlzyckwtfqjunsie
+mbruvapdghxlryckvtfejonsie
+mbrzvapdghxlzvcbwtfqjonsie
+mbruvapdgqxlzyckwcfqjonsce
+abruvupdrhxlzyckwtfqjonsie
+mbrubaptghxlzyckwtfqjondie
+mgruvapdgpxlzyckwtfijonsie
+mbruvapdghxczlckwtfujonsie
+mbruvapdgmmlzyckwtfqjonsir
+mbruvapdhhxltyckwtfdjonsie
+mbruvapdghxlzyckwtfdjjnste
+mbrdvzpdghxlcyckwtfqjonsie
+mbruvapdghxlzyckwtnqbonsim
+mbrovapdghxlzyckwtfpjousie
+mymuvapdghxlzyjkwtfqjonsie
+mbpuvapdghxlzyckwtfljcnsie
+mbrxvapdghxlzyclwtfqjonpie
+mbrueapdghxlzyckwtfqjopsia
+mbruvapdghxlzycdwtfqjbfsie
+tbruvavdghxlzyckwtmqjonsie
+mbduvapdghxlzyckwrfqjrnsie
+mkrsvapughxlzyckwtfqjonsie
+mbruvapdghylzyckwtfqtolsie
+mgruvapdglxldyckwtfqjonsie
+mbrunapdghclzyckwtfqjonsiy
+mbruvapdgrxlxyckwtfgjonsie
+mbruvapdghxpzbckftfqjonsie
+mbruvcpdghxyzyckotfqjonsie
+mbruvapdghxlsyckwtfqcqnsie
+mbruvapdghxlzzckwtfqjonskf
+mbruvppdghxlzfckwtfqjgnsie
+mbhuvapdghxlzytkwtfqjonoie
+mbruvapdghxlzvrkwtfqjjnsie
+mbmuvapdghxuzyckwtfqjonsze
+mbruvapdghnlzycnwtfqjonsil
+mbruvapdgholzyckitfqjonsia
+mbruxapdghxlmyckwtfqbonsie
+mbauvapdgholzyckwtfqjolsie
+mbruvapdghxlzyckwtfqjotslq
+dbrutapdghxlzyckwtfqjonsiv
+mbruvapdzhxlyyckwtfbjonsie
+mmruaapsghxlzyckwtfqjonsie
+mbruvaldgqxqzyckwtfqjonsie
+mbruvaodghxdzyjkwtfqjonsie
+mbrcmatdghxlzyckwtfqjonsie
+mbrqvapdgtxlzycewtfqjonsie
+mjruvapdghzlzyckwtfqjonrie
+mbruvapdghxopcckwtfqjonsie
+mbruvapdghxszycwwtfqjoqsie
+mbruvapdgoxezyckwtjqjonsie`
+/*
+abcdef contains no letters that appear exactly two or three times. -> 0 0
+bababc contains two a and three b, so it counts for both. -> 1 1
+abbcde contains two b, but no letter appears exactly three times. ->  1 0
+abcccd contains three c, but no letter appears exactly two times.  0 1
+aabcdd contains two a and two d, but it only counts once. 1 0
+abcdee contains two e. 1 0
+ababab contains three a and three b, but it only counts once. 0 1
+
+Of these box IDs, four of them contain a letter which appears exactly twice, and three of them contain a letter which appears exactly three times. Multiplying these together produces a checksum of 4 * 3 = 12.
+
+What is the checksum for your list of box IDs?
+*/
+
+function indexWords(word) {
+    /*
+    takes char code
+    indexes in an array
+    checks an array per index
+    */
+    const arr = new Array(256);
+    arr.fill(0);
+    word.split('').map((b) => {
+        return arr[b.charCodeAt()] += 1
+    })
+    return [arr.includes(2) ? 1 : 0, arr.includes(3) ? 1 : 0]
+}
+
+
+sample = `abcdef
+bababc
+abbcde
+abcccd
+aabcdd
+abcdee
+ababab`
+
+// const assert =  (a,b)=>console.log(a===b)
+assert(indexWords('abcdef').reduce((a, b) => a + b) == 0)
+assert(indexWords('bababc').reduce((a, b) => a + b) == 2)
+assert(indexWords('abbcde').reduce((a, b) => a + b) == 1)
+
+function part1(words) {
+    var two = 0
+    var three = 0
+    for (const w of words) {
+        [a, b] = indexWords(w)
+        two += a
+        three += b
+    }
+    return two * three
+}
+console.log(part1(input.split('\n')))
+
+// `part 2`
+const countWord = (word) => {
+    return word.split().reduce((a, b) => a + b.charCodeAt(), 0)
+}
+const counts = input.split('\n').map(countWord)
+function part2(words) {
+
+    for (const w of words) {
+        for (const w2 of words) {
+            s = w2.split('').map((v, k) => {
+                return v === w[k]
+            }).reduce((a, b) => {
+                if (b === false) {
+                    return a + 1
+                }
+                return a
+            }, 0)
+            // console.log(s)
+            if (s === 1) {
+                console.log(w, w2)
+            }
+        }
+    }
+}
+
+console.log(counts.slice(0, 10))
+console.log(part2(input.split('\n')))
